4be6a3c20cb047beb67bb6b8a3722cbc94caadf9
roshan-jose-bounteous <roshanjose.aj@bounteous.com>
2024-10-04 18:27:18 +0530

    Fix: Production-2, PDP page

--- src/components/plp/ProductList/ProductList.tsx
+++ src/components/plp/ProductList/ProductList.tsx
@@ -8,9 +8,7 @@ const ProductList = () => {
     <div className="flex flex-col justify-center px-36 py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
         {productDetails.map((product, index) => (
-          <div>
-            <ProductCard key={index} product={product} />
-          </div>
+          <ProductCard key={index} product={product} />
         ))}
       </div>
       <Pagination />
