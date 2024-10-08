1ecf24dc21e1a3b98df57730f3ba9ebebd40055e
roshan-jose-bounteous <roshanjose.aj@bounteous.com>
2024-10-08 18:45:54 +0530

    Static model-5

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -16,7 +16,7 @@ type ButtonProps = {
   children?: ReactNode;
   text?: string;
   onClick?: () => void;
-  disabled?: Boolean;
+  disabled?: boolean;
   className?: string; // Custom class for styling
 };
 
